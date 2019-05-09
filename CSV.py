e99bca496b0853c8883ee9047501b463a4970cf2
Xlmk <vh.dakov@gmail.com>
2019-05-09 21:00:52 +0300

    Collecting info from all my systems

---
+++ CSV.py
@@ -0,0 +1,8 @@
+path = "D:\Programming files and projects\Python\Google Stock Market Data - google_stock_data.csv.csv"
+
+#file = open(path)
+#for line in file:
+#    print(line)
+lines = [l for l in open(path)]
+lines[0]
+lines[1]
