e86b32eac12da38a7edbc980606c54fa8387ebb7
Val <vh.dakov@gmail.com>
2019-06-06 01:26:04 +0300

    06/06/2019 01:26:04& Updated

--- KeepUp.ps1
+++ KeepUp.ps1
@@ -1,5 +1,8 @@
-﻿git fetch
+﻿git add *
+git commit -m "$(get-date)& Updated"
+git fetch
 $result = git merge
 if ($result -like "Already up to date.") {
     Write-Output "Nothing to send"
 }
+git push
